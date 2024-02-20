b63e136b7e0f7fb3b23aaf51a815461dadc8d345
Nick Leavitt <nicholas.o.leavitt@maine.edu>
2024-02-20 13:43:05 -0500

    Pin Initialization

--- Lab_04/main.s
+++ Lab_04/main.s
@@ -19,6 +19,7 @@
 
 ; Green LED <--> PA.5
 LED_PIN	EQU	5
+BUTTON_PIN EQU 13
 	
 	AREA    main, CODE, READONLY
 	EXPORT	__main				; make __main visible to linker
@@ -32,7 +33,7 @@ __main	PROC
 	ORR r1, r1, #RCC_AHB2ENR_GPIOAEN
 	STR r1, [r0, #RCC_AHB2ENR]
 	
-	; Enable clock for GPIOC
+	; Enable clock for GPIO Port C
 	LDR r1, [r0, #RCC_AHB2ENR]
 	ORR r1, r1, #RCC_AHB2ENR_GPIOCEN
 	STR r1, [r0, #RCC_AHB2ENR]
@@ -48,6 +49,19 @@ __main	PROC
 	LDR r1, [r0, #GPIO_ODR]
 	ORR r1, r1, #(1<<LED_PIN)
 	STR r1, [r0, #GPIO_ODR]
+	
+	; MODE: 00: Input mode, 01: General purpose output mode
+    ;       10: Alternate function mode, 11: Analog mode (reset state)
+	LDR r0, =GPIOC_BASE
+	LDR r1, [r0, #GPIO_MODER]
+	BIC r1, r1, #(3<<(2*BUTTON_PIN))
+	STR r1, [r0, #GPIO_MODER]
+
+	LDR r1, [r0, #GPIO_PUPDR]
+	ORR r1, r1, #(3<<L(2*BUTTON_PIN))
+	STR r1, [r0, #GPIO_PUPDR]
+	
+	
   
 stop 	B 		stop     		; dead loop & program hangs here
 
