39ebfaa861db94dc535b282755cfa9d6b2e9e1d1
Nick Leavitt <nicholas.o.leavitt@maine.edu>
2024-02-28 13:39:58 -0500

    Better Comments, differentiated Lab code from cool code

--- Lab_05/main.s
+++ Lab_05/main.s
@@ -43,47 +43,48 @@ __main	PROC
 		
 	BL stepper_pin_init
 	BL butt_init
-	MOV r5, #0						;Initializing r5, I use this to determine the direction.
-start
-	LDR r0, =GPIOC_BASE
-	LDR r1, [r0, #GPIO_IDR]			;Load Idr
-	ROR r1, r1, #BUTTON_PIN			;Rotate until button pin is in the LSB position
-	BIC r1, r1, #0xfffffffe			;Clear everything but LSB
-	EOR r1, r1, #1					;Button is high when open, so this flips the bit
-	CMP r1, #1						;Compare LSB to #1
-	BEQ	pres						;If equal, go to pres//means the button was pressed
-	B	start
-loop
-pres
-	CMP r5, #0						;Checking r5 to determine direction of spin
-	BEQ	cw							;If 0, clockwise
-	B	ccw							;Else Counter Clockwise
-cw	
-	BL 	full_step_cw				;Runs only when button is held down
-	B	skip
-ccw	
-	BL	full_step_ccw				;Runs only when button is held down
-skip	
-	LDR r0, =GPIOC_BASE
-	LDR r1, [r0, #GPIO_IDR]			;Testing if the button is still pressed//same code as above
-	ROR r1, r1, #BUTTON_PIN
-	BIC r1, r1, #0xfffffffe	
-	EOR r1, r1, #1					
-	CMP r1, #1
-	BNE	break
-	B	loop
-break
-	EOR r5, r5, #1					;Flips r5, therefore changing direction
-	B	start
-
+;;;;-------------------------SOMETHING COOL-----------------------------------------------------
+;;;;	MOV r5, #0						;Initializing r5, I use this to determine the direction.
+;;;;start
+;;;;	LDR r0, =GPIOC_BASE
+;;;;	LDR r1, [r0, #GPIO_IDR]			;Load Idr
+;;;;	ROR r1, r1, #BUTTON_PIN			;Rotate until button pin is in the LSB position
+;;;;	BIC r1, r1, #0xfffffffe			;Clear everything but LSB
+;;;;	EOR r1, r1, #1					;Button is high when open, so this flips the bit
+;;;;	CMP r1, #1						;Compare LSB to #1
+;;;;	BEQ	pres						;If equal, go to pres//means the button was pressed
+;;;;	B	start
+;;;;loop
+;;;;pres
+;;;;	CMP r5, #0						;Checking r5 to determine direction of spin
+;;;;	BEQ	cw							;If 0, clockwise
+;;;;	B	ccw							;Else Counter Clockwise
+;;;;cw	
+;;;;	BL 	full_step_cw				;Runs only when button is held down
+;;;;	B	skip
+;;;;ccw	
+;;;;	BL	full_step_ccw				;Runs only when button is held down
+;;;;skip	
+;;;;	LDR r0, =GPIOC_BASE
+;;;;	LDR r1, [r0, #GPIO_IDR]			;Testing if the button is still pressed//same code as above
+;;;;	ROR r1, r1, #BUTTON_PIN
+;;;;	BIC r1, r1, #0xfffffffe	
+;;;;	EOR r1, r1, #1					
+;;;;	CMP r1, #1
+;;;;	BNE	break
+;;;;	B	loop
+;;;;break
+;;;;	EOR r5, r5, #1					;Flips r5, therefore changing direction
+;;;;	B	start
+;;;;-------------------------------------------------------------------------------------------------
 
 
 ;;;;--------WORKING LAB CODE---------
-;;;;	MOV r4, #FULL_360
-;;;;loop	
-;;;;	BL half_step
-;;;;	SUBS r4, #1
-;;;;	BNE	loop
+	MOV r4, #FULL_360
+loop	
+	BL full_step_cw
+	SUBS r4, #1
+	BNE	loop
 ;;;;---------------------------------
 
 stop 	B 		stop     		; dead loop & program hangs here
