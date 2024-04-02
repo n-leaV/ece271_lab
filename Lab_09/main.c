215b6ba2d87b0f168d1ca09f0194b0b4a09a30ff
Nick Leavitt <nicholas.o.leavitt@maine.edu>
2024-04-02 15:10:15 -0400

    Timer intialisations and interrupot handler

--- Lab_09/main.c
+++ Lab_09/main.c
@@ -3,11 +3,15 @@
 
 // PA.5  <--> Green LED
 // PC.13 <--> Blue user button
-#define LED_PIN    5
+#define B_PIN    6
 #define BUTTON_PIN 13
 
 void System_Clock_init(void);
+void GPIOB_pin_init(void);
 
+volatile int32_t pulse_width = 0;
+volatile int32_t last_captured = 0;
+volatile int32_t signal_polarity = 0;
 
 int main(void){
 
@@ -27,9 +31,86 @@ void System_Clock_init(void){
 	
 	while((RCC->CR & RCC_CR_HSIRDY) != RCC_CR_HSIRDY);
 	
-	RCC->CFGR &= RCC_CFGR_SW;
+	RCC->CFGR &= ~(RCC_CFGR_SW);
 	RCC->CFGR |= RCC_CFGR_SW_1;
+}
+
+void GPIOB_pin_init(void){
+	RCC->AHB2ENR |= RCC_AHB2ENR_GPIOBEN;	// Enable the clock of Port B
 	
+	GPIOA->MODER &= ~(3<<(2*B_PIN)); // Clear mode bits for pin 6
+	GPIOA->MODER	|= (2<<(2*B_PIN)); 	//set the mode bits to 10 - alternate function
 	
+	GPIOA->AFR[0] &= ~(0xf<<(4*B_PIN));	//selecting alternate function 1
+	GPIOA->AFR[0] |= 2UL<<(4*B_PIN);	//TIM4 channel 1 is defined as 02
+
+	GPIOA->OTYPER &= ~(1<<(B_PIN)); // Clear bit 5
+	GPIOA->OTYPER |= (0);			// Set OTYPER to push pull
+	
+	GPIOA->PUPDR &= ~(3<<(2*B_PIN));	//Clear bits 11 and 10
+	GPIOA->PUPDR |= (0);			//set PUPDR to no pullup/nopulldown
+	
+	GPIOA->ODR &= ~(1<<(B_PIN));  // clears bit 5 of ODR/set bit 5 to zero
+}
+
+
+
+void TIM4_CH1_Init() {
+	RCC->APB1ENR1 |= RCC_APB1ENR1_TIM4EN;		//enable timer clock
+	
+	TIM4->CR1 &= ~TIM_CR1_DIR; //select up counting
+	
+	TIM4->PSC = 15;		//16MHz/(1+PSC) = 1 MHz   ->PSC=15
+	
+	TIM4->ARR = 0xffff;			//Maximum ARR value
+	
+	//TIM4->CCR1 = LED_CCR1;						//inital duty cycle of 50%
+	
+	TIM4->CCMR1 &= ~TIM_CCMR1_CC1S;		// clear capture/compare mode bits
+	TIM4->CCMR1 |= TIM_CCMR1_CC1S_0;	// set input timer 1
+	
+	TIM4->CCMR1 &= ~TIM_CCMR1_IC1F;  // No filtering
+	
+	TIM4->CCER |= TIM_CCER_CC1P|TIM_CCER_CC1NP; // Both edges generate interrupts
+	
+	TIM4->CCMR1 &= ~(TIM_CCMR1_IC1PSC);		//clear psc and capture each transition
+	
+	TIM4->CCER |= TIM_CCER_CC1E;			//enable capture for channel 1
+	
+	TIM4->DIER |= TIM_DIER_CC1IE;		//allow TIM4 channel 1 to generate interrupts
+	
+	TIM4->DIER |= TIM_DIER_CC1DE; // allow TIM4 channel 1 to generate DMA requests
+
+	TIM4->CR1 |= TIM_CR1_CEN;		//enable the timer counter
+
+	NVIC_SetPriority(TIM4_IRQn, 0); //set to 0, highest priority
+
+	NVIC_EnableIRQ(TIM4_IRQn);		// enable TIM4 in the interrupt controller
+
+	//while(1)
+}
+
+
+void TIM4_IRQHandler(){
+	
+	uint32_t current_captured;
+	
+	if((TIM4->SR & TIM_SR_CC1IF) != 0) {
+		current_captured = TIM4->CCR1;
+		
+		signal_polarity = 1 - signal_polarity;
+		
+		if(signal_polarity == 0){
+			pulse_width = current_captured - last_captured;
+			if(pulse_width < 0)
+				pulse_width = pulse_width + TIM4->ARR;
+		}
+		
+		last_captured = current_captured;
+		
+	}
+	if((TIM4->SR & TIM_SR_UIF) != 0){
+		TIM4->SR &= ~TIM_SR_UIF;
+	}
 	
 }
