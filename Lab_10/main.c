f79e1e0b47d9ca342704c62224f4bdcb2af8b541
Nick Leavitt <nicholas.o.leavitt@maine.edu>
2024-04-09 22:15:02 -0400

    led and timer initializations

--- Lab_10/main.c
+++ Lab_10/main.c
@@ -135,3 +135,132 @@ void SysTick_Handler(void){
 		TimeDelay--;
 	
 }
+
+void configure_LED_pin(){
+  // Enable the clock to GPIO Port A and C
+  RCC->AHB2ENR |= RCC_AHB2ENR_GPIOAEN;
+	RCC->AHB2ENR |= RCC_AHB2ENR_GPIOCEN;
+	
+	//LED
+	// GPIO Mode: Input(00), Output(01), AlterFunc(10), Analog(11, reset)
+	GPIOA->MODER &= ~GPIO_MODER_MODE5;  
+	GPIOA->MODER |=  GPIO_MODER_MODE5_1;      // AlterFunc(10)
+	
+	// GPIO Speed: Low speed (00), Medium speed (01), Fast speed (10), High speed (11)
+	GPIOA->OSPEEDR &= ~GPIO_OSPEEDR_OSPEED5; //Low Speed
+	
+	// GPIO Output Type: Output push-pull (0, reset), Output open drain (1) 
+	GPIOA->OTYPER &= ~GPIO_OTYPER_ODR_5;      // Push-pull
+	
+	// GPIO Push-Pull: No pull-up, pull-down (00), Pull-up (01), Pull-down (10), Reserved (11)
+	GPIOA->PUPDR  &= ~GPIO_PUPDR_PUPDR5;  // No pull-up, no pull-down
+	
+	//Servo
+		// GPIO Mode: Input(00), Output(01), AlterFunc(10), Analog(11, reset)
+	GPIOA->MODER &= ~GPIO_MODER_MODE0;  
+	GPIOA->MODER |=  GPIO_MODER_MODE0_1;      // AlterFunc(10)
+	
+	// GPIO Speed: Low speed (00), Medium speed (01), Fast speed (10), High speed (11)
+	GPIOA->OSPEEDR &= ~GPIO_OSPEEDR_OSPEED5; //Low Speed
+	
+	// GPIO Output Type: Output push-pull (0, reset), Output open drain (1) 
+	GPIOA->OTYPER &= ~GPIO_OTYPER_ODR_5;      // Push-pull
+	
+	// GPIO Push-Pull: No pull-up, pull-down (00), Pull-up (01), Pull-down (10), Reserved (11)
+	GPIOA->PUPDR  &= ~GPIO_PUPDR_PUPDR5;  // No pull-up, no pull-down
+	
+	//BUTTON
+	//Sets the GPIO mode to input (00), no value line required after
+	GPIOC->MODER &= ~GPIO_MODER_MODE13; 
+
+	//Sets GPIOC to no pull-up, no pull-down
+	GPIOC->PUPDR &= ~GPIO_PUPDR_PUPD13; 
+	
+	//Configure Alternate Function
+	GPIOA->AFR[0] &= ~GPIO_AFRL_AFSEL5;
+	GPIOA->AFR[0] &= ~GPIO_AFRL_AFSEL0;
+	
+	GPIOA->AFR[0] |= GPIO_AFRL_AFSEL5_0;
+	GPIOA->AFR[0] |= GPIO_AFRL_AFSEL0_1;
+	
+}
+
+//LED
+void TIM2_CH1_Init(){
+	
+	//Enable Timer Clock
+	RCC->APB1ENR1 |= RCC_APB1ENR1_TIM2EN;
+	
+	//Counting Direction
+	TIM2->CR1 &= ~TIM_CR1_DIR;
+	
+	//Prescaler
+	TIM2->PSC = 39;
+	
+	//Auto Reload
+	TIM2->ARR = 999;
+	
+	//Output Compare Register
+	TIM2-> CCR1 = 500;
+	
+	//Clear output comepare mode
+	TIM2->CCMR1 &=  ~TIM_CCMR1_OC1M;
+	
+	//Select PWM mode 1
+	TIM2->CCMR1 |= TIM_CCMR1_OC1M_1 | TIM_CCMR1_OC1M_2;
+	
+	//Output 1 Enable
+	TIM2->CCMR1 |= TIM_CCMR1_OC1PE;
+	
+	//Select output polarity
+	TIM2->CCER &= ~TIM_CCER_CC1P;
+	
+	//Enable the output of channel
+	TIM2->CCER |= TIM_CCER_CC1E;
+	
+	//Main output enable (MOE)
+	TIM2->BDTR |= TIM_BDTR_MOE;
+	
+	//Start the counter
+	TIM2->CR1 |= TIM_CR1_CEN;
+}
+
+//Servo
+void TIM5_CH1_Init(){
+	
+	//Enable Timer Clock
+	RCC->APB1ENR1 |= RCC_APB1ENR1_TIM5EN;
+	
+	//Set timer to counting up
+	TIM5->CR1 &= ~TIM_CR1_DIR;
+	
+	//Prescaler to slow down input clock
+	TIM5->PSC = 7;
+	
+	//Auto Reload
+	TIM5->ARR = 9999;
+	
+	//Output Compare Register, Initial Duty Cycle
+	TIM5-> CCR1 = 4999;
+	
+	//Clear output compare mode, PWM mode 1 due to counting up
+	TIM5->CCMR1 &=  ~TIM_CCMR1_OC1M;
+	
+	//Allows the ARR value to be counted up to
+	TIM5->CCMR1 |= TIM_CCMR1_OC1M_1 | TIM_CCMR1_OC1M_2;
+	
+	//Output 1 Enable
+	TIM5->CCMR1 |= TIM_CCMR1_OC1PE;
+	
+	//Select output polarity to 0, because we want the signal to be doing something at a high value
+	TIM5->CCER &= ~TIM_CCER_CC1P;
+	
+	//Enable the output of channel 1
+	TIM5->CCER |= TIM_CCER_CC1E;
+	
+	//Main output enable (MOE)
+	TIM5->BDTR |= TIM_BDTR_MOE;
+	
+	//Start the counter
+	TIM5->CR1 |= TIM_CR1_CEN;
+}
